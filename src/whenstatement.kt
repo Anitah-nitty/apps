3de0a72bce48c9ce0b4cbced128e1241c6b56e41
Anitah-nitty <anitahreh856@gmail.com>
2026-03-26 11:52:49 +0300

    new commit

--- src/whenstatement.kt
+++ src/whenstatement.kt
@@ -1,8 +1,7 @@
 fun main() {
     var day = 4
 
-    var result = when(day){
-
+    var dayResult = when(day){
         1 -> "Monday"
         2 -> "Tuesday"
         3 -> "Wednesday"
@@ -12,6 +11,24 @@ fun main() {
         7 -> "Sunday"
         else -> "Invalid day of the week"
     }
-    println("Today is $result")
+    println("Today is $dayResult")
 
+    var month = 7
+
+    var monthResult = when(month){
+        1 -> "January"
+        2 -> "February"
+        3 -> "March"
+        4 -> "April"
+        5 -> "May"
+        6 -> "June"
+        7 -> "July"
+        8 -> "August"
+        9 -> "September"
+        10 -> "October"
+        11 -> "November"
+        12 -> "December"
+        else -> "Invalid month"
+    }
+    println("Birthday month is $monthResult")
 }
